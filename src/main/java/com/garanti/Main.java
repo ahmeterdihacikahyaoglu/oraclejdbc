8e93e35bf93d69d8191d8b63e30d90139bdfcd44
Ahmet Erdi Hacıkahyaoğlu <ahmet-erdi@hotmail.com>
2022-12-21 11:01:59 +0300

    İlk commit, bütün kodlar

---
+++ src/main/java/com/garanti/Main.java
@@ -0,0 +1,47 @@
+package com.garanti;
+
+import com.garanti.model.*;
+import com.garanti.repo.*;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        OgretmenRepo ogretmenRepo = new OgretmenRepo();
+        /*ArrayList<Ogretmen> ogretmenler = ogretmenRepo.getAll();
+        System.err.println(ogretmenler.toString());
+        System.err.println(ogretmenRepo.getById(4));*/
+        /*if (ogretmenRepo.deleteById(5)) {
+            System.err.println("Başarı ile silindi");
+        } else {
+            System.err.println("Başarı ile silinemedi");
+        }*/
+        /*if (ogretmenRepo.save(new Ogretmen("Deniz", true))) {
+            System.err.println("Başarı ile kaydedildi");
+        } else {
+            System.err.println("Başarı ile kaydedilemedi");
+        }*/
+
+        /*OgrenciRepo ogrenciRepo = new OgrenciRepo();
+        ArrayList<Ogrenci> ogrenciler = ogrenciRepo.getAll();
+        System.err.println(ogrenciler.toString());
+        System.err.println(ogrenciRepo.getById(1));*/
+
+        /*KonuRepo konuRepo = new KonuRepo();
+        ArrayList<Konu> konular = konuRepo.getAll();
+        System.err.println(konular.toString());
+        System.err.println(konuRepo.getById(1));*/
+
+        /*Ders_OgrenciRepo dersOgrenciRepo = new Ders_OgrenciRepo();
+        ArrayList<Ders_Ogrenci> dersOgrenci = dersOgrenciRepo.getAll();
+        System.err.println(dersOgrenci.toString());
+        System.err.println(dersOgrenciRepo.getById(6));*/
+
+        /*DersRepo dersRepo = new DersRepo();
+        ArrayList<Ders> dersler = dersRepo.getAll();
+        System.err.println(dersler.toString());
+        System.err.println(dersRepo.getById(7));*/
+
+        System.err.println(ogretmenRepo.getAllLike("a"));
+    }
+}
