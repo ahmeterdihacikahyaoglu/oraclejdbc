d6454d85d43413255fc702cca5bf447fa457047a
Ahmet Erdi Hacıkahyaoğlu <78377927+ahmetterdii@users.noreply.github.com>
2022-12-21 19:59:06 +0300

    Update Bolme.java

--- src/main/java/com/garanti/test/Bolme.java
+++ src/main/java/com/garanti/test/Bolme.java
@@ -3,5 +3,6 @@ package com.garanti.test;
 public class Bolme {
     public static void asd() {
         System.err.println("Bolme işlemi sınıfı çalışıyor....");
+        System.err.println("Bolme işlemi sınıfı çalıştı ve kaydedildi.....");
     }
 }
